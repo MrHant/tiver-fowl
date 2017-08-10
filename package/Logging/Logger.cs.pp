d83ac5cfb200a5dfb733f7e15171bd8a45fc6d69
Artyom Silivonchik <mr.hant@gmail.com>
2017-08-10 10:49:18 +0300

    Add Logger and TestNameEnricher to package

---
+++ package/Logging/Logger.cs.pp
@@ -0,0 +1,26 @@
+namespace $rootnamespace$.Logging
+{
+    using Serilog;
+    using Serilog.Formatting.Json;
+
+    public class Logger
+    {
+        public static void Configure()
+        {
+            if (configured)
+            {
+                return;
+            }
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .Enrich.With(new TestNameEnricher())
+                .WriteTo.LiterateConsole()
+                .WriteTo.File(new JsonFormatter(), "./log.txt")
+                .CreateLogger();
+            configured = true;
+        }
+
+        private static bool configured;
+    }
+}
